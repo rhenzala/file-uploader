abe69550dd22c2a7caaef1748545636f0d603591
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-03-14 00:01:00 +0800

    Update routes for login and register

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,8 +7,14 @@ module.exports = (supabase) => {
   router.get('/', (req, res) => res.render('index'));
   router.get('/login', (req, res) => res.render('index'));
   router.post('/login', passport.authenticate('local', { successRedirect: '/files', failureRedirect: '/' }));
-  router.get('/register', (req, res) => res.render('register'));
+  router.get('/register', (req, res) => res.render('index', {auth: 'register'}));
   router.post('/register', (req, res) => register(req, res, supabase));
+  router.get('/loginPartial', (req, res) => {
+    res.render('login');
+  });
+  router.get('/registerPartial', (req, res) => {
+    res.render('register');
+  });
   router.get('/logout', logout);
   return router;
 };
